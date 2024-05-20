a3905b3a01124e816208547d91bb08780b3ac836
sidekicklx <lixuezixiang@gmail.com>
2024-05-20 04:09:14 +0000

    refactor

---
+++ prompts.py
@@ -0,0 +1,40 @@
+
+
+# Prompt for iterative auto program repair.
+ITERATIVE_PROMPTS = \
+"""
+    You are a cybersecurity expert that is responsible to repair a vulnerable code.
+    {code}
+    Your task is to generate a code snippet that fix the vulerability. Do not output other irrelavent information!
+    Here are some useful information that can help you identify the vulerability. There will be an 'N/A' if the information is not availiable.
+    Possible line number of the vulerability: {line_number}
+    The type of the Vulnerability : {vul_type}
+    You will also be provided with sample repairs from the previous iteration and the feedback from an auto-test program.
+    sample repair: {reapired_code}
+    feedback: {harness_fdbk}
+    Begin!
+"""
+
+# Prompt prefix for the agent 
+AGENT_PROMPT_PREFIX = \
+"""
+    You are a cybersecurity expert that is responsible to repair a vulnerable code.
+    {code}
+    Your task is to generate a code snippet that fix the vulerability. Do not output other irrelavent information!
+    You can use following tools {pr_tools}
+    Here are some useful information that can help you identify the vulerability. There will be an 'N/A' if the information is not availiable.
+    Possible line number of the vulerability: {line_number}
+    The type of the Vulnerability : {vul_type}
+    You will also be provided with sample repairs from the previous iteration and the feedback from an auto-test program.
+    sample repair: {reapired_code}
+    feedback: {harness_fdbk}
+"""
+
+# Prompt suffix for the agent
+AGENT_PROMPT_SUFFIX = \
+"""
+    Begin! Remember to answer very politely.
+    Previous conversation history:
+    {chat_history}
+    {agent_scratchpad}
+"""
--- utils.py
+++ utils.py
@@ -1,27 +1,39 @@
 import os
+from goalkeeper_test_patch import test_patch
 
 
-def gen_diff_file(source_file, target_file, filename):
+def gen_patch(project, repaired_func, patch_name):
+    """Generate patch file from source and target files"""
     try:
-        os.system(f"diff -u {source_file} {target_file} > {filename}")
-        return filename
+        with open(project.file_name, "r") as f:
+            lines = f.readlines()
+            # delete original function and insert the new one
+            del lines[project.line_number[0]:project.line_number[1]]
+            lines.insert(project.line_number[0], repaired_func.split("\n"))
+        with open(project.file_name, "w") as f:
+            f.write("\n".join(lines))
+        os.system(f"git add {project.file_name}")
+        os.system(f"git diff > {patch_name}") # TODO: handle file lists
+        return patch_name
     except Exception as e:
         print(e)
         return None
 
-def load_code_need_to_fix(input_json):
-    """
-    Load the code snippet from the input json
-    input_json: dict {
-        "files": ["file1,c", "file2.c"],
-        ""
 
+def goal_keeper(patch_name, tgt_proj="linux"):
+    # TODO: generate patch
+    with open(patch_name, 'r') as f:
+        patch = f.read()
+    feedback = test_patch(patch, tgt_proj)
+    '''
+    {
+    'build_status': build_status,
+    'build_output': build_output,
+    'pov_results': pov_results,
+    'test_status': test_status,
+    'test_output': test_output
     }
-    """
-    for file in input_json["files"]:
-        if file.endswith(".c"):
-            with open(file, "r") as f:
-                return f.read()
-    return 
+    '''
+    # TODO: wrapper
 
-
+    return feedback
